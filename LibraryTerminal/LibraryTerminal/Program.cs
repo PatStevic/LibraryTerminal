76d9be064847868e2f0e4c992d4b3120dc0ebb29
pstevic <patrickstevic@outlook.com>
2020-05-27 19:19:49 -0400

    empty commit

---
+++ LibraryTerminal/LibraryTerminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
