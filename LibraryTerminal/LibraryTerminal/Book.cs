5be3b0f65eb14a8576073be22f5fc2fdd2e17050
pstevic <patrickstevic@outlook.com>
2020-05-28 21:44:58 -0400

    broke down work

--- LibraryTerminal/LibraryTerminal/Book.cs
+++ LibraryTerminal/LibraryTerminal/Book.cs
@@ -9,5 +9,8 @@ namespace LibraryTerminal
         public string Title { get; set; }
         public string Author { get; set; }
         public BookStatus Status { get; set; }
+        public DateTime CheckOutDate {get; set;}
+        public DateTime DueDate { get; set; }
+
     }
 }
--- LibraryTerminal/LibraryTerminal/Program.cs
+++ LibraryTerminal/LibraryTerminal/Program.cs
@@ -9,16 +9,33 @@ namespace LibraryTerminal
         {
             Console.WriteLine("Welcome to our library!");
 
-            Book book1 = new Book()
-            {
-                Title = "",
-                Author = "",
-                Status = BookStatus
-        };
-
-
         }
-        //  
+        /*  Welcome the user to library
+         *  
+         *  Ask user which method they would like to use in program
+         *  Methods:
+         *  -display entire list of books neatley (1)
+         *      select a book
+         *          -display whether checked out or not
+         *              if checked in, offer option to checkout and show due date
+         *                  -save file
+         *              if checked out, display due date
+         *  -search for book by author(2)
+         *       -display whether checked out or not
+         *              if checked in, offer option to checkout and show due date
+         *                  -save file
+         *              if checked out, display due date
+         *  -search for book by title keyword
+         *       -display whether checked out or not
+         *              if checked in, offer option to checkout and show due date
+         *                  -save file
+         *              if checked out, display due date   
+         *  -return a book
+         *      -enter book title then confirm if user wants to return
+         *          -save file
+         *  
+
+        */
         //
         //
         //
