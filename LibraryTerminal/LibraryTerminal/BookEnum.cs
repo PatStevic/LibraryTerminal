620764b5504c86c8eb93d40c4a44f4949e37014a
kyle-nedbal <kylenedbal88@gmail.com>
2020-06-02 20:41:18 -0400

    adding classes to Mak's upload

--- LibraryTerminal/LibraryTerminal/BookEnum.cs
+++ LibraryTerminal/LibraryTerminal/BookEnum.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 namespace LibraryTerminal
 {
-    public class BookEnum
+    public enum BookEnum
     {
-        public BookEnum()
-        {
-        }
+        DiscoverYourCliftonStrengths = 1,
+        ThriftyYears = 2,
+        AstrophysicsForPeopleInAHurry = 3,
+        TheTanningOfAmerica = 4,
+        HowToWinFriendsAndInfluencePeople = 5,
+        TheSiren = 6,
+        AGameOfThrones = 7,
+        TheBitPicture = 8,
+        GabrielsInferno = 9,
+        TheEndOfOurStory = 10,
+        FirstFieldGuideAmphibians = 11,
+        StrengthsBasedLeadership = 12,
+        EverythingHappensForAReason = 13
     }
 }
